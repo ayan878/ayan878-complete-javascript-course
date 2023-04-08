9a563647983312d64d5fdff4fc2869ff78437265
Ayan Raza <ayanraza197@gmail.com>
2023-04-08 14:35:42 +0530

    Fundamental 1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -77,4 +77,5 @@ Use the BMI example from Challenge #1, and the code you already wrote, and impro
 1. Print a nice output to the console, saying who has the higher BMI. The message can be either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
 2. Use a template literal to include the BMI values in the outputs. Example: "Mark's BMI (28.3) is higher than John's (23.9)!"
 
-HINT: Use an if/else statement 😉*/
+HINT: Use an if/else statement 😉
+Challenge 2 completed above*/
